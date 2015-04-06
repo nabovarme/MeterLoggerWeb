29bf6569dd0da95ca3ac0589f72bd406663a1609
st0ff3r <stoffer@skulp.net>
2015-04-06 18:44:48 +0200

    + low res data/graph

--- perl/Nabovarme/Data.pm
+++ perl/Nabovarme/Data.pm
@@ -1,6 +1,7 @@
 package Nabovarme::Data;
 
 use strict;
+use Data::Dumper;
 use Apache2::RequestRec ();
 use Apache2::RequestIO ();
 use Apache2::Const;
@@ -14,21 +15,41 @@ sub handler {
 	my $r = shift;
 	my ($dbh, $sth, $d);
 	
-	my ($serial) = $r->uri =~ m|/(\d+)|;
+	my ($serial, $resolution) = $r->uri =~ m|/(\d+)(?:/(\d+))?|;
+	warn Dumper ($serial, $resolution);
 	
 	if ($dbh = Nabovarme::Db->my_connect) {
 		my $quoted_serial = $dbh->quote($serial);
-		$sth = $dbh->prepare(qq[SELECT \
-			DATE_FORMAT(FROM_UNIXTIME(unix_time), "%Y/%m/%d %T") AS time_stamp_formatted, \
-			serial, \
-			flow_temp, \
-			return_flow_temp, \
-			temp_diff, \
-			flow, \
-			effect, \
-			hours, \
-			volume, \
-			energy FROM samples WHERE `serial` LIKE ] . $quoted_serial . qq[ ORDER BY `unix_time` ASC]);
+		if ($resolution) {
+			$sth = $dbh->prepare(qq[SELECT \
+				DATE_FORMAT(FROM_UNIXTIME(unix_time), "%Y/%m/%d %T") AS `time_stamp_formatted`, \
+				serial, \
+				AVG(`flow_temp`) as `flow_temp`, \
+				AVG(`return_flow_temp`) as `return_flow_temp`, \
+				AVG(`temp_diff`) as `temp_diff`, \
+				AVG(`flow`) as `flow`, \
+				AVG(`effect`) as `effect`, \
+				`hours`, \
+				`volume`, \
+				`energy` \
+				FROM `samples` \
+				WHERE `serial` LIKE ] . $quoted_serial . qq[ \
+				GROUP BY DATE( FROM_UNIXTIME(`unix_time`) ), HOUR( FROM_UNIXTIME(`unix_time`) ) \
+				ORDER BY FROM_UNIXTIME(`unix_time`) ASC]);
+		}
+		else {
+			$sth = $dbh->prepare(qq[SELECT \
+				DATE_FORMAT(FROM_UNIXTIME(unix_time), "%Y/%m/%d %T") AS time_stamp_formatted, \
+				serial, \
+				flow_temp, \
+				return_flow_temp, \
+				temp_diff, \
+				flow, \
+				effect, \
+				hours, \
+				volume, \
+				energy FROM samples WHERE `serial` LIKE ] . $quoted_serial . qq[ ORDER BY `unix_time` ASC]);
+		}
 		$sth->execute;
 	}
 	
@@ -53,3 +74,5 @@ sub handler {
 }
 
 1;
+
+__END__
