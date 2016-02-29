88ce192953110fd4bcd9dac54de74708fc48cd53
st0ff3r <stoffer@skulp.net>
2016-03-01 00:24:30 +0100

    meter_grapher.pl updated from running version, added update_meters.pl

---
+++ update_meters.pl
@@ -0,0 +1,107 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Data::Dumper;
+use Sys::Syslog;
+use Net::MQTT::Simple;
+use DBI;
+
+#use lib qw( /var/www/perl/lib/ );
+use lib qw( /opt/local/apache2/perl/ );
+use Nabovarme::Db;
+
+openlog($0, "ndelay,pid", "local0");
+syslog('info', "starting...");
+
+my $unix_time;
+my $meter_serial;
+my $mqtt = Net::MQTT::Simple->new(q[loppen.christiania.org]);
+my $mqtt_data = undef;
+#my $mqtt_count = 0;
+
+# connect to db
+my $dbh;
+my $sth;
+my $d;
+
+if ($dbh = Nabovarme::Db->my_connect) {
+	$dbh->{'mysql_auto_reconnect'} = 1;
+	syslog('info', "connected to db");
+}
+else {
+	syslog('info', "cant't connect to db $!");
+	die $!;
+}
+
+$sth = $dbh->prepare(qq[SELECT `serial`,`info` FROM meters WHERE `serial` IN (SELECT DISTINCT(`serial`) `serial` FROM samples)]);
+$sth->execute;
+
+while ($d = $sth->fetchrow_hashref) {
+	print Dumper $d->{serial};
+	$mqtt->retain('/config/v1/' . $d->{serial} . '/version' => 'retain');
+	$mqtt->retain('/config/v1/' . $d->{serial} . '/status' => 'retain');
+}
+
+# end of main
+
+
+
+
+__END__
+
+unless ($topic =~ m!/sample/v1/(\d+)/(\d+)!) {
+	return;
+}
+$meter_serial = $1;
+$unix_time = $2;
+
+# parse message
+$message =~ s/&$//;
+
+my ($key, $value, $unit);
+my @key_value_list = split(/&/, $message);
+my $key_value; 
+foreach $key_value (@key_value_list) {
+	if (($key, $value, $unit) = $key_value =~ /([^=]*)=(\S+)(?:\s+(.*))?/) {
+		# check energy register unit
+		if ($key =~ /^e1$/i) {
+			if ($unit =~ /^MWh$/i) {
+				$value *= 1000;
+				$unit = 'kWh';
+			}
+		}
+		$mqtt_data->{$key} = $value;
+	}
+}
+warn Dumper($mqtt_data);
+
+# save to db
+if ($unix_time < time() + 7200) {
+	my $sth = $dbh->prepare(qq[INSERT INTO `samples` (
+		`serial`,
+		`heap`,
+		`flow_temp`,
+		`return_flow_temp`,
+		`temp_diff`,
+		`flow`,
+		`effect`,
+		`hours`,
+		`volume`,
+		`energy`,
+		`unix_time`
+		) VALUES (] . 
+		$dbh->quote($meter_serial) . ',' . 
+		$dbh->quote($mqtt_data->{heap}) . ',' . 
+		$dbh->quote($mqtt_data->{t1}) . ',' . 
+		$dbh->quote($mqtt_data->{t2}) . ',' . 
+		$dbh->quote($mqtt_data->{tdif}) . ',' . 
+		$dbh->quote($mqtt_data->{flow1}) . ',' . 
+		$dbh->quote($mqtt_data->{effect1}) . ',' . 
+		$dbh->quote($mqtt_data->{hr}) . ',' . 
+		$dbh->quote($mqtt_data->{v1}) . ',' . 
+		$dbh->quote($mqtt_data->{e1}) . ',' .
+		$dbh->quote($unix_time) . qq[)]);
+	$sth->execute || syslog('info', "can't log to db");
+	$sth->finish;
+}
+$mqtt_data = undef;
