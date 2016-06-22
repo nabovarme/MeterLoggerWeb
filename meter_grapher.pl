dc41540afc832a76724f4b988d01d576df0430fe
st0ff3r <stoffer@skulp.net>
2016-06-22 08:45:59 +0200

    meter_grapher.pl
    support hmac sha256 on both topic and message

--- meter_grapher.pl
+++ meter_grapher.pl
@@ -40,9 +40,9 @@ else {
 
 # start mqtt run loop
 $mqtt->run(	q[/sample/v2/#] => \&sample_mqtt_handler,
-	q[/xversion/v1/#] => \&mqtt_version_handler,
-	q[/xstatus/v1/#] => \&mqtt_status_handler,
-	q[/xuptime/v1/#] => \&mqtt_uptime_handler,
+	q[/version/v2/#] => \&mqtt_version_handler,
+	q[/status/v2/#] => \&mqtt_status_handler,
+	q[/uptime/v2/#] => \&mqtt_uptime_handler,
 	q[/crypto/v2/#] => \&mqtt_crypto_test_handler
 );
 
@@ -50,59 +50,153 @@ $mqtt->run(	q[/sample/v2/#] => \&sample_mqtt_handler,
 
 sub mqtt_version_handler {
 	my ($topic, $message) = @_;
-	unless ($topic =~ m!/version/v1/(\d+)/(\d+)!) {
-		return;
+	my $m;
+
+	unless ($topic =~ m!/version/v\d+/(\d+)/(\d+)!) {
+	        return;
 	}
-	$sw_version = $message;
 	$meter_serial = $1;
 	$unix_time = $2;
-	
-	my $quoted_sw_version = $dbh->quote($sw_version);
-	my $quoted_meter_serial = $dbh->quote($meter_serial);
-	my $quoted_unix_time = $dbh->quote($unix_time);
-	$dbh->do(qq[UPDATE meters SET \
-					sw_version = $quoted_sw_version, \
-					last_updated = $quoted_unix_time \
-					WHERE serial = $quoted_meter_serial]) or warn $!;
-	warn Dumper({sw_version => $sw_version});
+
+	$message =~ /(.{32})(.{16})(.+)/s;
+	my $mac = $1;
+	my $iv = $2;
+	my $ciphertext = $3;
+		
+	my $sth = $dbh->prepare(qq[SELECT `key` FROM meters WHERE serial = ] . $dbh->quote($meter_serial) . qq[ LIMIT 1]);
+	$sth->execute;
+	if ($sth->rows) {
+		my $key = $sth->fetchrow_hashref->{key} || warn "no aes key found";
+		my $sha256 = sha256(pack('H*', $key));
+		my $aes_key = substr($sha256, 0, 16);
+		my $hmac_sha256_key = substr($sha256, 16, 16);
+
+		warn Dumper("message: " . unpack('H*', $message));
+		warn Dumper("aes key: " . unpack('H*', $aes_key));
+		warn Dumper("hmac sha256 key: " . unpack('H*', $hmac_sha256_key));
+		warn Dumper("iv: " . unpack('H*', $iv));
+		warn Dumper("ciphertext: " . unpack('H*', $ciphertext));
+		warn Dumper("mac: " . unpack('H*', $mac));
+		
+		if ($mac eq hmac_sha256($iv . $ciphertext . $topic, $hmac_sha256_key)) {
+			# hmac sha256 ok
+			$m = Crypt::Mode::CBC->new('AES');
+			$sw_version = $m->decrypt($ciphertext, $aes_key, $iv);
+			my $quoted_sw_version = $dbh->quote($sw_version);
+			my $quoted_meter_serial = $dbh->quote($meter_serial);
+			my $quoted_unix_time = $dbh->quote($unix_time);
+			$dbh->do(qq[UPDATE meters SET \
+							sw_version = $quoted_sw_version, \
+							last_updated = $quoted_unix_time \
+							WHERE serial = $quoted_meter_serial]) or warn $!;
+			warn Dumper({sw_version => $sw_version});
+		}
+		else {
+			# hmac sha256 not ok
+			warn Dumper("serial $meter_serial checksum error");
+		}
+	}
 }
 
 sub mqtt_status_handler {
 	my ($topic, $message) = @_;
-	unless ($topic =~ m!/status/v1/(\d+)/(\d+)!) {
+	my $m;
+	
+	unless ($topic =~ m!/status/v\d+/(\d+)/(\d+)!) {
 		return;
 	}
-	$valve_status = $message;
 	$meter_serial = $1;
 	$unix_time = $2;
 
-	my $quoted_valve_status = $dbh->quote($valve_status);
-	my $quoted_meter_serial = $dbh->quote($meter_serial);
-	my $quoted_unix_time = $dbh->quote($unix_time);
-	$dbh->do(qq[UPDATE meters SET \
-					valve_status = $quoted_valve_status, \
-					last_updated = $quoted_unix_time \
-					WHERE serial = $quoted_meter_serial]) or warn $!;
-	warn Dumper({sw_version => $valve_status});
+	$message =~ /(.{32})(.{16})(.+)/s;
+	my $mac = $1;
+	my $iv = $2;
+	my $ciphertext = $3;
+		
+	my $sth = $dbh->prepare(qq[SELECT `key` FROM meters WHERE serial = ] . $dbh->quote($meter_serial) . qq[ LIMIT 1]);
+	$sth->execute;
+	if ($sth->rows) {
+		my $key = $sth->fetchrow_hashref->{key} || warn "no aes key found";
+		my $sha256 = sha256(pack('H*', $key));
+		my $aes_key = substr($sha256, 0, 16);
+		my $hmac_sha256_key = substr($sha256, 16, 16);
+
+		warn Dumper("message: " . unpack('H*', $message));
+		warn Dumper("aes key: " . unpack('H*', $aes_key));
+		warn Dumper("hmac sha256 key: " . unpack('H*', $hmac_sha256_key));
+		warn Dumper("iv: " . unpack('H*', $iv));
+		warn Dumper("ciphertext: " . unpack('H*', $ciphertext));
+		warn Dumper("mac: " . unpack('H*', $mac));
+		
+		if ($mac eq hmac_sha256($iv . $ciphertext . $topic, $hmac_sha256_key)) {
+			# hmac sha256 ok
+			$m = Crypt::Mode::CBC->new('AES');
+			$valve_status = $m->decrypt($ciphertext, $aes_key, $iv);
+			my $quoted_valve_status = $dbh->quote($valve_status);
+			my $quoted_meter_serial = $dbh->quote($meter_serial);
+			my $quoted_unix_time = $dbh->quote($unix_time);
+			$dbh->do(qq[UPDATE meters SET \
+							valve_status = $quoted_valve_status, \
+							last_updated = $quoted_unix_time \
+							WHERE serial = $quoted_meter_serial]) or warn $!;
+			warn Dumper({valve_status => $valve_status});
+			
+		}
+		else {
+			# hmac sha256 not ok
+			warn Dumper("serial $meter_serial checksum error");
+		}
+	}
 }
 
 sub mqtt_uptime_handler {
 	my ($topic, $message) = @_;
-	unless ($topic =~ m!/uptime/v1/(\d+)/(\d+)!) {
+	my $m;
+	
+	unless ($topic =~ m!/uptime/v\d+/(\d+)/(\d+)!) {
 		return;
 	}
-	$uptime = $message;
 	$meter_serial = $1;
 	$unix_time = $2;
 	
-	my $quoted_uptime = $dbh->quote($uptime);
-	my $quoted_meter_serial = $dbh->quote($meter_serial);
-	my $quoted_unix_time = $dbh->quote($unix_time);
-	$dbh->do(qq[UPDATE meters SET \
-					uptime = $quoted_uptime, \
-					last_updated = $quoted_unix_time \
-					WHERE serial = $quoted_meter_serial]) or warn $!;
-	warn Dumper({sw_version => $sw_version});
+	$message =~ /(.{32})(.{16})(.+)/s;
+	my $mac = $1;
+	my $iv = $2;
+	my $ciphertext = $3;
+		
+	my $sth = $dbh->prepare(qq[SELECT `key` FROM meters WHERE serial = ] . $dbh->quote($meter_serial) . qq[ LIMIT 1]);
+	$sth->execute;
+	if ($sth->rows) {
+		my $key = $sth->fetchrow_hashref->{key} || warn "no aes key found";
+		my $sha256 = sha256(pack('H*', $key));
+		my $aes_key = substr($sha256, 0, 16);
+		my $hmac_sha256_key = substr($sha256, 16, 16);
+
+		warn Dumper("message: " . unpack('H*', $message));
+		warn Dumper("aes key: " . unpack('H*', $aes_key));
+		warn Dumper("hmac sha256 key: " . unpack('H*', $hmac_sha256_key));
+		warn Dumper("iv: " . unpack('H*', $iv));
+		warn Dumper("ciphertext: " . unpack('H*', $ciphertext));
+		warn Dumper("mac: " . unpack('H*', $mac));
+		
+		if ($mac eq hmac_sha256($iv . $ciphertext . $topic, $hmac_sha256_key)) {
+			# hmac sha256 ok
+			$m = Crypt::Mode::CBC->new('AES');
+			$uptime = $m->decrypt($ciphertext, $aes_key, $iv);
+			my $quoted_uptime = $dbh->quote($uptime);
+			my $quoted_meter_serial = $dbh->quote($meter_serial);
+			my $quoted_unix_time = $dbh->quote($unix_time);
+			$dbh->do(qq[UPDATE meters SET \
+							uptime = $quoted_uptime, \
+							last_updated = $quoted_unix_time \
+							WHERE serial = $quoted_meter_serial]) or warn $!;
+			warn Dumper({uptime => $uptime});
+		}
+		else {
+			# hmac sha256 not ok
+			warn Dumper("serial $meter_serial checksum error");
+		}
+	}
 }
 
 sub sample_mqtt_handler {
@@ -139,7 +233,7 @@ sub sample_mqtt_handler {
 		warn Dumper("ciphertext: " . unpack('H*', $ciphertext));
 		warn Dumper("mac: " . unpack('H*', $mac));
 		
-		if ($mac eq hmac_sha256($iv . $ciphertext, $hmac_sha256_key)) {
+		if ($mac eq hmac_sha256($iv . $ciphertext . $topic, $hmac_sha256_key)) {
 			# hmac sha256 ok
 			$m = Crypt::Mode::CBC->new('AES');
 			$message = $m->decrypt($ciphertext, $aes_key, $iv);
@@ -198,16 +292,9 @@ sub sample_mqtt_handler {
 		}
 		else {
 			# hmac sha256 not ok
+			warn Dumper("serial $meter_serial checksum error");
 		}
-		
-		
-
 	}
-
-
-
-	
-
 	$mqtt_data = undef;
 }
 
@@ -242,7 +329,7 @@ sub mqtt_crypto_test_handler {
 		warn Dumper("ciphertext: " . unpack('H*', $ciphertext));
 		warn Dumper("mac: " . unpack('H*', $mac));
 		
-		if ($mac eq hmac_sha256($iv . $ciphertext, $hmac_sha256_key)) {
+		if ($mac eq hmac_sha256($iv . $ciphertext . $topic, $hmac_sha256_key)) {
 			# hmac sha256 ok
 			$m = Crypt::Mode::CBC->new('AES');
 			$message = $m->decrypt($ciphertext, $aes_key, $iv);
@@ -251,6 +338,7 @@ sub mqtt_crypto_test_handler {
 		}
 		else {
 			# hmac sha256 not ok
+			warn Dumper("serial $meter_serial checksum error");
 		}
 	}
 }
