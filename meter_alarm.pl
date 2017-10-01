1b92c36c097790c3e0026728c83a31abdb0c1b2a
st0ff3r <stoffer@skulp.net>
2017-10-02 01:50:22 +0200

    optimize sql select

--- meter_alarm.pl
+++ meter_alarm.pl
@@ -86,7 +86,7 @@ sub check_conditions {
 					my $values = [];
 					my $median;
 					my $sth_down_message_vars = $dbh->prepare(qq[SELECT ] . $quoted_down_message_var . qq[ FROM `samples` \
-																WHERE `serial` like ] . $quoted_serial . qq[ ORDER BY `unix_time` DESC LIMIT 3]);
+																WHERE `serial` like ] . $quoted_serial . qq[ AND FROM_UNIXTIME(`unix_time`) > NOW() - INTERVAL 1 DAY ORDER BY `unix_time` DESC LIMIT 3]);
 					$sth_down_message_vars->execute;
 					if ($sth_down_message_vars->rows) {
 						$values = $sth_down_message_vars->fetchall_arrayref;
@@ -110,7 +110,7 @@ sub check_conditions {
 					my $values = [];
 					my $median;
 					my $sth_up_message_vars = $dbh->prepare(qq[SELECT ] . $quoted_up_message_var . qq[ FROM `samples` \
-																WHERE `serial` like ] . $quoted_serial . qq[ ORDER BY `unix_time` DESC LIMIT 3]);
+																WHERE `serial` like ] . $quoted_serial . qq[ AND FROM_UNIXTIME(`unix_time`) > NOW() - INTERVAL 1 DAY ORDER BY `unix_time` DESC LIMIT 3]);
 					$sth_up_message_vars->execute;
 					if ($sth_up_message_vars->rows) {
 						$values = $sth_up_message_vars->fetchall_arrayref;
@@ -134,7 +134,7 @@ sub check_conditions {
 					my $values = [];
 					my $median;
 					my $sth_condition_vars = $dbh->prepare(qq[SELECT ] . $quoted_condition_var . qq[ FROM `samples` \
-																WHERE `serial` like ] . $quoted_serial . qq[ ORDER BY `unix_time` DESC LIMIT 3]);
+																WHERE `serial` like ] . $quoted_serial . qq[ AND FROM_UNIXTIME(`unix_time`) > NOW() - INTERVAL 1 DAY ORDER BY `unix_time` DESC LIMIT 3]);
 					$sth_condition_vars->execute;
 					if ($sth_condition_vars->rows) {
 						$values = $sth_condition_vars->fetchall_arrayref;
