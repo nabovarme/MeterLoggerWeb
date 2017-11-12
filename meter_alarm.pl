21f14466d17da3a1361a7294007871fbb8be7ff3
st0ff3r <stoffer@skulp.net>
2017-11-12 17:34:39 +0100

    calculate median based on 5 samples not 3

--- meter_alarm.pl
+++ meter_alarm.pl
@@ -86,7 +86,7 @@ sub check_conditions {
 					my $values = [];
 					my $median;
 					my $sth_down_message_vars = $dbh->prepare(qq[SELECT ] . $quoted_down_message_var . qq[ FROM `samples_cache` \
-																WHERE `serial` like ] . $quoted_serial . qq[ ORDER BY `unix_time` DESC LIMIT 3]);
+																WHERE `serial` like ] . $quoted_serial . qq[ ORDER BY `unix_time` DESC LIMIT 5]);
 					$sth_down_message_vars->execute;
 					if ($sth_down_message_vars->rows) {
 						$values = $sth_down_message_vars->fetchall_arrayref;
