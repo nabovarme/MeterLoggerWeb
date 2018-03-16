e6b38457543782ff4166078b18e9d9350a80ed56
st0ff3r <stoffer@skulp.net>
2018-03-16 04:36:16 +0100

    updated meter_wifi_config.pl to use set_ssid_pwd for newer firmware
    >= 1032
    
    deletes username/pass from db if status is connected
    
    better timing in sending mqtt commands

--- meter_wifi_config.pl
+++ meter_wifi_config.pl
@@ -17,8 +17,8 @@ use lib qw( /etc/apache2/perl );
 use lib qw( /opt/local/apache2/perl/ );
 use Nabovarme::Db;
 
-use constant DELAY_AFTER_SENDING => 200_000;			# 200 mS
-use constant DELAY_AFTER_SENDING_RECONNECT => 30_000_000;	# 30 seconds
+use constant DELAY_AFTER_SENDING => 1_000_000;				# 1 second
+use constant DELAY_AFTER_SENDING_RECONNECT => 12_000_000;	# 12 seconds
 
 $SIG{INT} = \&sig_int_handler;
 
@@ -35,11 +35,7 @@ my $mqtt_data = undef;
 
 my $dbh;
 my $sth;
-my $sth_kwh_left;
-my $sth_valve_status;
 my $d;
-my $d_kwh_left;
-my $d_valve_status;
 
 my $key;
 my $m;
@@ -75,38 +71,81 @@ sub start_thread {
 	my $aes_key = substr($sha256, 0, 16);
 	my $hmac_sha256_key = substr($sha256, 16, 16);
 	
+	my $dbh_recheck;
+	my $sth_recheck;
+	my $d_recheck;
+	# connect to db
+	if ($dbh_recheck = Nabovarme::Db->my_connect) {
+		$dbh_recheck->{'mysql_auto_reconnect'} = 1;
+		syslog('info', "connected to db in thread");
+	}
+	else {
+		syslog('info', "cant't connect to db $!");
+		die $!;
+	}
+	
 	$m = Crypt::Mode::CBC->new('AES');
 
-	if (defined($_[0]->{wifi_set_ssid})) {
-		# send set_ssid
-		warn "\tsend mqtt command to change ssid to " . $_[0]->{serial};
-		syslog('info', "\tsend mqtt command to change ssid to " . $_[0]->{serial});
-		$topic = '/config/v2/' . $_[0]->{serial} . '/' . time() . '/set_ssid';
-		$message = $_[0]->{wifi_set_ssid} . "\0";
-		$iv = join('', map(chr(int rand(256)), 1..16));
-		$message = $m->encrypt($message, $aes_key, $iv);
-		$message = $iv . $message;
-		$hmac_sha256_hash = hmac_sha256($topic . $message, $hmac_sha256_key);
-		$mqtt->publish($topic => $hmac_sha256_hash . $message);
-		usleep(DELAY_AFTER_SENDING);	
+	$_[0]->{sw_version} =~ /[^-]+-(\d+)/;
+	if ($1 >= 1032) {
+		# use combined set_ssid and set_pwd for newer firmware
+		if (defined($_[0]->{wifi_set_ssid}) && defined($_[0]->{wifi_set_pwd})) {
+			# send set_ssid_pwd
+			warn "\tsend mqtt command to change ssid to " . $_[0]->{wifi_set_ssid} . " to " . $_[0]->{serial};
+			syslog('info', "    send mqtt command to change ssid to " . $_[0]->{wifi_set_ssid} . " to " . $_[0]->{serial});
+			# escape & and =
+			$_[0]->{serial} =~ s/&/%26/g;
+			$_[0]->{serial} =~ s/=/%61/g;
+			$_[0]->{wifi_set_pwd} =~ s/&/%26/g;
+			$_[0]->{wifi_set_pwd} =~ s/=/%61/g;
+			$topic = '/config/v2/' . $_[0]->{serial} . '/' . time() . '/set_ssid_pwd';
+			$message = "ssid=" . $_[0]->{wifi_set_ssid} . "&pwd=" . $_[0]->{wifi_set_pwd} . "\0";
+			$iv = join('', map(chr(int rand(256)), 1..16));
+			$message = $m->encrypt($message, $aes_key, $iv);
+			$message = $iv . $message;
+			$hmac_sha256_hash = hmac_sha256($topic . $message, $hmac_sha256_key);
+			$mqtt->publish($topic => $hmac_sha256_hash . $message);
+			warn "\twaiting " . DELAY_AFTER_SENDING / 1_000_000 . " seconds for reply";
+			syslog('info', "    waiting " . DELAY_AFTER_SENDING / 1_000_000 . " seconds for reply");
+			usleep(DELAY_AFTER_SENDING);	
+		}
 	}
-	if (defined($_[0]->{wifi_set_pwd})) {
-		# send set_pwd
-		warn "\tsend mqtt command to change wifi pwd to " . $_[0]->{serial};
-		syslog('info', "\tsend mqtt command to change wifi pwd to " . $_[0]->{serial});
-		$topic = '/config/v2/' . $_[0]->{serial} . '/' . time() . '/set_pwd';
-		$message = $_[0]->{wifi_set_pwd} . "\0";
-		$iv = join('', map(chr(int rand(256)), 1..16));
-		$message = $m->encrypt($message, $aes_key, $iv);
-		$message = $iv . $message;
-		$hmac_sha256_hash = hmac_sha256($topic . $message, $hmac_sha256_key);
-		$mqtt->publish($topic => $hmac_sha256_hash . $message);
-		usleep(DELAY_AFTER_SENDING);
+	else {
+		if (defined($_[0]->{wifi_set_ssid})) {
+			# send set_ssid
+			warn "\tsend mqtt command to change ssid to " . $_[0]->{wifi_set_ssid} . " to " . $_[0]->{serial};
+			syslog('info', "    send mqtt command to change ssid to " . $_[0]->{wifi_set_ssid} . " to " . $_[0]->{serial});
+			$topic = '/config/v2/' . $_[0]->{serial} . '/' . time() . '/set_ssid';
+			$message = $_[0]->{wifi_set_ssid} . "\0";
+			$iv = join('', map(chr(int rand(256)), 1..16));
+			$message = $m->encrypt($message, $aes_key, $iv);
+			$message = $iv . $message;
+			$hmac_sha256_hash = hmac_sha256($topic . $message, $hmac_sha256_key);
+			$mqtt->publish($topic => $hmac_sha256_hash . $message);
+			warn "\twaiting " . DELAY_AFTER_SENDING / 1_000_000 . " seconds for reply";
+			syslog('info', "    waiting " . DELAY_AFTER_SENDING / 1_000_000 . " seconds for reply");
+			usleep(DELAY_AFTER_SENDING);	
+		}
+		if (defined($_[0]->{wifi_set_pwd})) {
+			# send set_pwd
+			warn "\tsend mqtt command to change wifi pwd to " . $_[0]->{serial};
+			syslog('info', "    send mqtt command to change wifi pwd to " . $_[0]->{serial});
+			$topic = '/config/v2/' . $_[0]->{serial} . '/' . time() . '/set_pwd';
+			$message = $_[0]->{wifi_set_pwd} . "\0";
+			$iv = join('', map(chr(int rand(256)), 1..16));
+			$message = $m->encrypt($message, $aes_key, $iv);
+			$message = $iv . $message;
+			$hmac_sha256_hash = hmac_sha256($topic . $message, $hmac_sha256_key);
+			$mqtt->publish($topic => $hmac_sha256_hash . $message);
+			warn "\twaiting " . DELAY_AFTER_SENDING / 1_000_000 . " seconds for reply";
+			syslog('info', "    waiting " . DELAY_AFTER_SENDING / 1_000_000 . " seconds for reply");
+			usleep(DELAY_AFTER_SENDING);
+		}
 	}
 	
 	# send reconnect
 	warn "\tsend mqtt command to reconnect to " . $_[0]->{serial};
-	syslog('info', "\tsend mqtt command to reconnect to " . $_[0]->{serial});
+	syslog('info', "    send mqtt command to reconnect to " . $_[0]->{serial});
 	$topic = '/config/v2/' . $_[0]->{serial} . '/' . time() . '/reconnect';
 	$message = '';
 	$iv = join('', map(chr(int rand(256)), 1..16));
@@ -114,23 +153,13 @@ sub start_thread {
 	$message = $iv . $message;
 	$hmac_sha256_hash = hmac_sha256($topic . $message, $hmac_sha256_key);
 	$mqtt->publish($topic => $hmac_sha256_hash . $message);
+	warn "\twaiting " . DELAY_AFTER_SENDING_RECONNECT / 1_000_000 . " seconds for reconnect";
+	syslog('info', "    waiting " . DELAY_AFTER_SENDING_RECONNECT / 1_000_000 . " seconds for reconnect");
 	usleep(DELAY_AFTER_SENDING_RECONNECT);
-	
-	# send ssid
-	warn "\tsend mqtt command to get ssid to" . $_[0]->{serial};
-	syslog('info', "\tsend mqtt command to get ssid to" . $_[0]->{serial});
-	$topic = '/config/v2/' . $_[0]->{serial} . '/' . time() . '/ssid';
-	$message = '';
-	$iv = join('', map(chr(int rand(256)), 1..16));
-	$message = $m->encrypt($message, $aes_key, $iv);
-	$message = $iv . $message;
-	$hmac_sha256_hash = hmac_sha256($topic . $message, $hmac_sha256_key);
-	$mqtt->publish($topic => $hmac_sha256_hash . $message);
-	usleep(DELAY_AFTER_SENDING);
 	
 	# send wifi_status
 	warn "\tsend mqtt command to get wifi status to " . $_[0]->{serial};
-	syslog('info', "\tsend mqtt command to get wifi status to " . $_[0]->{serial});
+	syslog('info', "    send mqtt command to get wifi status to " . $_[0]->{serial});
 	$topic = '/config/v2/' . $_[0]->{serial} . '/' . time() . '/wifi_status';
 	$message = '';
 	$iv = join('', map(chr(int rand(256)), 1..16));
@@ -138,14 +167,39 @@ sub start_thread {
 	$message = $iv . $message;
 	$hmac_sha256_hash = hmac_sha256($topic . $message, $hmac_sha256_key);
 	$mqtt->publish($topic => $hmac_sha256_hash . $message);
+	warn "\twaiting " . DELAY_AFTER_SENDING / 1_000_000 . " seconds for reply";
+	syslog('info', "    waiting " . DELAY_AFTER_SENDING / 1_000_000 . " seconds for reply");
 	usleep(DELAY_AFTER_SENDING);
 	
-	if ($_[0]->{wifi_status} =~ /^connected/i) {
+	# send ssid
+	warn "\tsend mqtt command to get ssid to" . $_[0]->{serial};
+	syslog('info', "    send mqtt command to get ssid to " . $_[0]->{serial});
+	$topic = '/config/v2/' . $_[0]->{serial} . '/' . time() . '/ssid';
+	$message = '';
+	$iv = join('', map(chr(int rand(256)), 1..16));
+	$message = $m->encrypt($message, $aes_key, $iv);
+	$message = $iv . $message;
+	$hmac_sha256_hash = hmac_sha256($topic . $message, $hmac_sha256_key);
+	$mqtt->publish($topic => $hmac_sha256_hash . $message);
+	warn "\twaiting " . DELAY_AFTER_SENDING / 1_000_000 . " seconds for reply";
+	syslog('info', "    waiting " . DELAY_AFTER_SENDING / 1_000_000 . " seconds for reply");
+	usleep(DELAY_AFTER_SENDING);
+	
+	# re-check wifi_status
+	my $wifi_status;
+	my $quoted_serial = $dbh_recheck->quote($_[0]->{serial});
+	$sth_recheck = $dbh_recheck->prepare(qq[SELECT `wifi_status` FROM meters WHERE `serial` = $quoted_serial]);
+	$sth_recheck->execute;
+	
+	if ($d_recheck = $sth_recheck->fetchrow_hashref) {
+		$wifi_status = $d_recheck->{wifi_status};
+	}
+	if ($wifi_status =~ /^connected/i) {
 		# we asume that supplied ssid/pwd make MeterLogger able to connect, so we can delete it from the db
-		warn "done changing network for " . $_[0]->{serial} . "\n";
-		syslog('info', "done changing network for " . $_[0]->{serial});
-		my $quoted_meter_serial = $dbh->quote($_[0]->{serial});
-		$dbh->do(qq[UPDATE meters SET \
+		warn "done changing network to " . $_[0]->{wifi_set_ssid} . " for " . $_[0]->{serial};
+		syslog('info', "done changing network to " . $_[0]->{wifi_set_ssid} . " for " . $_[0]->{serial});
+		my $quoted_meter_serial = $dbh_recheck->quote($_[0]->{serial});
+		$dbh_recheck->do(qq[UPDATE meters SET \
 						wifi_set_ssid = NULL, \
 						wifi_set_pwd = NULL \
 						WHERE serial = $quoted_meter_serial]) or warn $!;
@@ -155,7 +209,7 @@ sub start_thread {
 
 while (1) {
 	my @threads = ();
-	$sth = $dbh->prepare(qq[SELECT `serial`, `wifi_status`, `wifi_set_ssid`, `wifi_set_pwd`, `key` FROM meters WHERE `wifi_status` = 'reconnect']);
+	$sth = $dbh->prepare(qq[SELECT `serial`, `wifi_status`, `wifi_set_ssid`, `wifi_set_pwd`, `key`, `sw_version` FROM meters WHERE `wifi_status` = 'reconnect']);
 	$sth->execute;
 	
 	while ($d = $sth->fetchrow_hashref) {
@@ -165,7 +219,7 @@ while (1) {
 	foreach (@threads) {
 		my $t = $_->join;
 	}
-	sleep 10;
+	sleep 20;
 }
 
 sub sig_int_handler {
