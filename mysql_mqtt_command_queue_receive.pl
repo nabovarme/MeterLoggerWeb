240b8535137828ef6482a89569baea1d85ba4ddc
st0ff3r <stoffer@skulp.net>
2018-03-23 23:25:11 +0100

    handle scan_result right
    just remove from db

--- mysql_mqtt_command_queue_receive.pl
+++ mysql_mqtt_command_queue_receive.pl
@@ -77,6 +77,22 @@ sub mqtt_handler {
 	if ($function =~ /^config$/i) {
 		return;
 	}
+
+	if ($function =~ /^open_until/i) {
+		warn "received mqtt reply from $meter_serial: $function, deleting from mysql queue\n";
+		syslog('info', "received mqtt reply from $meter_serial: $function, deleting from mysql queue");
+		# do mysql stuff here
+		warn Dumper(qq[DELETE FROM command_queue WHERE `serial` = ] . $dbh->quote($meter_serial) . qq[ AND `function` LIKE ] . $dbh->quote($function)) or warn $!;
+		$dbh->do(qq[DELETE FROM command_queue WHERE `serial` = ] . $dbh->quote($meter_serial) . qq[ AND `function` LIKE ] . $dbh->quote($function)) or warn $!;
+		return;
+	}
+	elsif ($function =~ /^scan_result$/i) {
+		warn "received mqtt reply from $meter_serial: $function, deleting from mysql queue\n";
+		syslog('info', "received mqtt reply from $meter_serial: $function, deleting from mysql queue");
+		# do mysql stuff here
+		$dbh->do(qq[DELETE FROM command_queue WHERE `serial` = ] . $dbh->quote($meter_serial) . qq[ AND `function` LIKE 'scan']) or warn $!;
+		return;
+	}
 	
 	$sth = $dbh->prepare(qq[SELECT `key`, `sw_version` FROM meters WHERE serial = ] . $dbh->quote($meter_serial) . qq[ LIMIT 1]);
 	$sth->execute;
@@ -109,35 +125,27 @@ sub mqtt_handler {
 				return;
 			}
 
-			if ($function =~ /^open_until/i) {
-				warn "received mqtt reply from $meter_serial: $function, with param: $cleartext, deleting from mysql queue\n";
-				syslog('info', "received mqtt reply from $meter_serial: $function, with param: $cleartext, deleting from mysql queue");
-				# do mysql stuff here
-				$dbh->do(qq[DELETE FROM command_queue WHERE `serial` = ] . $dbh->quote($meter_serial) . qq[ AND `function` LIKE ] . $dbh->quote($function)) or warn $!;
-			}
-			else {
-				$sth = $dbh->prepare(qq[SELECT `serial` FROM command_queue WHERE serial = ] . $dbh->quote($meter_serial) . qq[ AND `function` LIKE ] . $dbh->quote($function) . qq[ LIMIT 1]);
+			$sth = $dbh->prepare(qq[SELECT `serial` FROM command_queue WHERE serial = ] . $dbh->quote($meter_serial) . qq[ AND `function` LIKE ] . $dbh->quote($function) . qq[ LIMIT 1]);
 
-				# mark it for deletion
-				$sth = $dbh->prepare(qq[SELECT `id`, `has_callback` FROM command_queue \
-					WHERE `serial` = ] . $dbh->quote($meter_serial) . qq[ AND `function` LIKE ] . $dbh->quote($function) . qq[ AND `state` = 'sent']);
-				$sth->execute;
-				if ($d = $sth->fetchrow_hashref) {
-					if ($d->{has_callback}) {
-						warn "mark serial $meter_serial, command $function for deletion\n";
-						syslog('info', "mark serial $meter_serial, command $function for deletion");
-						$dbh->do(qq[UPDATE command_queue SET \
-							`state` = 'received', \
-						    `param` = ] . $dbh->quote($cleartext) . qq[ \
-							WHERE `id` = ] . $d->{id}) or warn $!;
-					}
-					else {
-						# delete if has_callback not set
-						warn "deleting serial $meter_serial, command $function from mysql queue\n";
-						syslog('info', "deleting serial $meter_serial, command $function from mysql queue");
-#						$dbh->do(qq[DELETE FROM command_queue WHERE `id` = ] . $d->{id});
-						$dbh->do(qq[DELETE FROM command_queue WHERE `serial` = ] . $dbh->quote($meter_serial) . qq[ AND `function` LIKE ] . $dbh->quote($function) . qq[ AND `state` = 'sent']);
-					}
+			# mark it for deletion
+			$sth = $dbh->prepare(qq[SELECT `id`, `has_callback` FROM command_queue \
+				WHERE `serial` = ] . $dbh->quote($meter_serial) . qq[ AND `function` LIKE ] . $dbh->quote($function) . qq[ AND `state` = 'sent']);
+			$sth->execute;
+			if ($d = $sth->fetchrow_hashref) {
+				if ($d->{has_callback}) {
+					warn "mark serial $meter_serial, command $function for deletion\n";
+					syslog('info', "mark serial $meter_serial, command $function for deletion");
+					$dbh->do(qq[UPDATE command_queue SET \
+						`state` = 'received', \
+					    `param` = ] . $dbh->quote($cleartext) . qq[ \
+						WHERE `id` = ] . $d->{id}) or warn $!;
+				}
+				else {
+					# delete if has_callback not set
+					warn "deleting serial $meter_serial, command $function from mysql queue\n";
+					syslog('info', "deleting serial $meter_serial, command $function from mysql queue");
+#					$dbh->do(qq[DELETE FROM command_queue WHERE `id` = ] . $d->{id});
+					$dbh->do(qq[DELETE FROM command_queue WHERE `serial` = ] . $dbh->quote($meter_serial) . qq[ AND `function` LIKE ] . $dbh->quote($function) . qq[ AND `state` = 'sent']);
 				}
 			}
 		}
