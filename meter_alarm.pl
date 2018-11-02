6c41af6f3efb4e409e209a12e3020d474391c124
st0ff3r <stoffer@skulp.net>
2018-11-02 01:26:20 +0100

    Eval working

--- meter_alarm.pl
+++ meter_alarm.pl
@@ -51,6 +51,7 @@ sub check_conditions {
 		my $condition;
 		my $condition_var;
 		my @condition_vars;
+		my $eval_alarm_state;
 		my $down_message_var;
 		my @down_message_vars;
 		my $up_message_var;
@@ -135,57 +136,65 @@ sub check_conditions {
 					my $median;
 					my $sth_condition_vars = $dbh->prepare(qq[SELECT ] . $quoted_condition_var . qq[ FROM `samples_cache` \
 																WHERE `serial` like ] . $quoted_serial . qq[ ORDER BY `unix_time` DESC LIMIT 3]);
-					$sth_condition_vars->execute;
-					if ($sth_condition_vars->rows) {
-						$values = $sth_condition_vars->fetchall_arrayref;
-						$median = median(@$values) + 0.0;	# hack to convert , to .
-						# replace symbol with value from database
-						$condition =~ s/\$$condition_var/$median/x;
-						$down_message =~ s/\$$condition_var/$median/x;
-						$up_message =~ s/\$$condition_var/$median/x;
+					if ($sth_condition_vars->execute) {
+						if (!$@ and $sth_condition_vars->rows) {
+							$values = $sth_condition_vars->fetchall_arrayref;
+							$median = median(@$values) + 0.0;	# hack to convert , to .
+							# replace symbol with value from database
+							$condition =~ s/\$$condition_var/$median/x;
+							$down_message =~ s/\$$condition_var/$median/x;
+							$up_message =~ s/\$$condition_var/$median/x;
+						}
 					}
 				}				
 			}
 			
 			syslog('info', "checking condition for serial $serial: $condition");
 			warn "checking condition for serial $serial: $condition";
-			if (eval $condition) {
-				# condition met, an alarm should be sent if we not dit it in the latest repeat time interval
-				if ($d->{alarm_state} == 0) {
-					# changed from no alarm to alarm - send sms
-					sms_send($d->{sms_notification}, $down_message);
-					$dbh->do(qq[UPDATE alarms SET \
-									last_notification = ] . time() . qq[, \
-									alarm_state = 1 \
-									WHERE `serial` like $quoted_serial]) or warn $!;
-					syslog('info', "serial $serial: down");
-					warn "down\n";
-				}
-				elsif ($d->{repeat} && (($d->{last_notification} + $d->{repeat}) < time())) {
-					# repeat down notification every 'repeat' time interval
-					sms_send($d->{sms_notification}, $down_message);
-					$dbh->do(qq[UPDATE alarms SET \
-									last_notification = ] . time() . qq[, \
-									alarm_state = 1 \
-									WHERE `serial` like $quoted_serial]) or warn $!;
-					syslog('info', "serial $serial: down repeat");
-					warn "down repeat\n";
-				}
-#				print "true\n";
+			$eval_alarm_state = eval 'no strict; ' . $condition;
+			if ($@) {
+				syslog('info', "error parsing condition for serial $serial: $condition, error: $@");
+				warn "error parsing condition for serial $serial: $condition, error: $@";
 			}
 			else {
-				# condition not met
-				if ($d->{alarm_state} == 1) {
-					# changed from alarm to no alarm - send sms
-					sms_send($d->{sms_notification}, $up_message);
-					syslog('info', "serial $serial: up");
-					warn "up\n";
+				if ($eval_alarm_state) {
+					# condition met, an alarm should be sent if we not dit it in the latest repeat time interval
+					if ($d->{alarm_state} == 0) {
+						# changed from no alarm to alarm - send sms
+						sms_send($d->{sms_notification}, $down_message);
+						$dbh->do(qq[UPDATE alarms SET \
+										last_notification = ] . time() . qq[, \
+										alarm_state = 1 \
+										WHERE `serial` like $quoted_serial]) or warn $!;
+						syslog('info', "serial $serial: down");
+						warn "down\n";
+					}
+					elsif ($d->{repeat} && (($d->{last_notification} + $d->{repeat}) < time())) {
+						# repeat down notification every 'repeat' time interval
+						sms_send($d->{sms_notification}, $down_message);
+						$dbh->do(qq[UPDATE alarms SET \
+										last_notification = ] . time() . qq[, \
+										alarm_state = 1 \
+										WHERE `serial` like $quoted_serial]) or warn $!;
+						syslog('info', "serial $serial: down repeat");
+						warn "down repeat\n";
+					}
+#					print "true\n";
 				}
-				$dbh->do(qq[UPDATE alarms SET \
-								last_notification = ] . time() . qq[, \
-								alarm_state = 0 \
-								WHERE `serial` like $quoted_serial]) or warn $!;
-#				print "false\n";
+				else {
+					# condition not met
+					if ($d->{alarm_state} == 1) {
+						# changed from alarm to no alarm - send sms
+						sms_send($d->{sms_notification}, $up_message);
+						syslog('info', "serial $serial: up");
+						warn "up\n";
+					}
+					$dbh->do(qq[UPDATE alarms SET \
+									last_notification = ] . time() . qq[, \
+									alarm_state = 0 \
+									WHERE `serial` like $quoted_serial]) or warn $!;
+#					print "false\n";
+				}
 			}
 		}
 	}
