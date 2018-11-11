9387a2c4fc6323e5781ebfabd26d688dea5f6144
Kristoffer Ek <stoffer@skulp.net>
2018-11-12 00:38:44 +0100

    missing startup.pl

---
+++ perl/startup.pl
@@ -0,0 +1,11 @@
+use lib qw(/etc/apache2/perl);
+
+use Embperl;
+use Data::Dumper;
+use DBI;
+use Nabovarme::Data;
+use Nabovarme::NetworkData;
+use Nabovarme::Redirect;
+use Nabovarme::SMSAuth;
+
+1;
