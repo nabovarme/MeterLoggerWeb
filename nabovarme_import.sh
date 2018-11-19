f883a5714420d15f95b6cf33a0fd6f59ed5dd93f
Kristoffer Ek <stoffer@skulp.net>
2018-11-19 19:25:19 +0100

    Update nabovarme_import.sh

--- nabovarme_import.sh
+++ nabovarme_import.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-mysql -u root -p$MYSQL_ROOT_PASSWORD nabovarme < /nabovarme.sql &&
+mysql -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD nabovarme < /nabovarme.sql &&
 
-echo "GRANT SELECT,INSERT,UPDATE,DELETE ON nabovarme.* TO 'nabovarme'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'" | mysql -u root -p$MYSQL_ROOT_PASSWORD &&
+echo "GRANT SELECT,INSERT,UPDATE,DELETE ON nabovarme.* TO 'nabovarme'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'" | mysql -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD &&
 
-echo "FLUSH PRIVILEGES" | mysql -u root -p$MYSQL_ROOT_PASSWORD
+echo "FLUSH PRIVILEGES" | mysql -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD
+
+mysql_upgrade -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD
