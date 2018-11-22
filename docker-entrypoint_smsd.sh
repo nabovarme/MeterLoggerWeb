25702d0882e09daa640959b65c112bb2327fdcad
Kristoffer Ek <stoffer@skulp.net>
2018-11-22 03:36:26 +0100

    updated smsd to use network bridged modem
    moved dockerfile commands to docker-entrypoint-smsd.sh
    
    device = /tmp/ttyV0

---
+++ docker-entrypoint_smsd.sh
@@ -0,0 +1,20 @@
+#/bin/sh
+
+mkdir -p /var/spool/sms/checked &&
+mkdir -p /var/spool/sms/failed &&
+mkdir -p /var/spool/sms/incoming &&
+mkdir -p /var/spool/sms/outgoing &&
+mkdir -p /var/spool/sms/sent &&
+chown -R smsd:smsd /var/spool/sms &&
+
+#daemon -r -n socat 
+socat -d -d pty,link=/tmp/ttyV0,raw,echo=0,wait-slave,b19200 tcp:10.0.1.186:9999 &
+while [ ! -L /tmp/ttyV0 ]
+	do sleep 1
+done
+ls -al /tmp/ &&
+smsd -c/etc/smsd.conf && tail -f /var/log/smstools/smsd.log &
+while [ -L /tmp/ttyV0 ]
+	do sleep 10
+done
+exit 1
