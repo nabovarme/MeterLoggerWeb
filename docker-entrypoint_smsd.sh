a34e60dbef33bed9713ec4a733d23e0a5fbb9cb3
Kristoffer Ek <stoffer@skulp.net>
2018-11-22 04:32:54 +0100

    use SMSD_HOST and SMSD_PORT for serial port

--- docker-entrypoint_smsd.sh
+++ docker-entrypoint_smsd.sh
@@ -8,7 +8,7 @@ mkdir -p /var/spool/sms/sent &&
 chown -R smsd:smsd /var/spool/sms &&
 
 #daemon -r -n socat 
-socat -d -d pty,link=/tmp/ttyV0,raw,echo=0,wait-slave,b19200 tcp:10.0.1.186:9999 &
+socat -d -d pty,link=/tmp/ttyV0,raw,echo=0,wait-slave,b19200 tcp:$SMSD_HOST:$SMSD_PORT &
 while [ ! -L /tmp/ttyV0 ]
 	do sleep 1
 done
