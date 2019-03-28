c6a91692d6af8108295c2b6a8f2427dc88931566
Kristoffer Ek <stoffer@skulp,net>
2019-03-28 02:04:15 +0100

    Create Crypto.pm
    decrypt_topic_message_for_serial() and cached lookup of key for serial

---
+++ perl/Nabovarme/Crypto.pm
@@ -0,0 +1,73 @@
+package Nabovarme::Crypto;
+
+use strict;
+use warnings;
+use DBI;
+use Crypt::Mode::CBC;
+use Digest::SHA qw( sha256 hmac_sha256 );
+use Data::Dumper;
+use Sys::Syslog;
+
+use lib qw( /etc/apache2/perl );
+#use lib qw( /opt/local/apache2/perl );
+use Nabovarme::Db;
+
+use constant CONFIG_FILE => qw (/etc/Nabovarme.conf );
+
+my $config = new Config::Simple(CONFIG_FILE) || die $!;
+my $config_cached_time = $config->param('cached_time') || 3600; # default 1 hour
+
+sub new {
+	my $class = shift;
+
+	my $self = {};
+	$self->{dbh} = Nabovarme::Db->my_connect || die $!;
+	$self->{key_cache} = undef;
+
+	return bless $self, $class;
+}
+
+sub decrypt_topic_message_for_serial {
+	my ($self, $topic, $message, $meter_serial) = @_;
+	$message =~ /(.{32})(.{16})(.+)/s;
+
+	my $key = '';
+	my $mac = $1;
+	my $iv = $2;
+	my $ciphertext = $3;
+
+	if (exists($self->{key_cache}->{$meter_serial}) && ($self->{key_cache}->{$meter_serial}->{cached_time} > (time() - $config_cached_time))) {
+		$key = $self->{key_cache}->{$meter_serial}->{key};
+	}
+	else {
+		my $sth = $self->{dbh}->prepare(qq[SELECT `key` FROM meters WHERE serial = ] . $self->{dbh}->quote($meter_serial) . qq[ LIMIT 1]);
+		$sth->execute;
+		if ($sth->rows) {
+			my $d = $sth->fetchrow_hashref;
+			$self->{key_cache}->{$meter_serial}->{key} = $d->{key};
+			$self->{key_cache}->{$meter_serial}->{cached_time} = time();
+
+			$key = $d->{key} || warn "no aes key found\n";
+		}
+	}
+
+	if ($key) {
+		my $sha256 = sha256(pack('H*', $key));
+		my $aes_key = substr($sha256, 0, 16);
+		my $hmac_sha256_key = substr($sha256, 16, 16);
+		
+		if ($mac eq hmac_sha256($topic . $iv . $ciphertext, $hmac_sha256_key)) {
+			# hmac sha256 ok
+			my $m = Crypt::Mode::CBC->new('AES');
+			my $cleartext = $m->decrypt($ciphertext, $aes_key, $iv);
+			# remove trailing nulls
+			$cleartext =~ s/[\x00\s]+$//;
+			$cleartext .= '';
+			return $cleartext;
+		}
+	}
+
+	return '';
+}
+
+1;
