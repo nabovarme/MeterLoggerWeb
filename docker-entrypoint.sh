99cf73538e7986112b3ce4c63e894c96f4b02bcc
Kristoffer Ek <stoffer@skulp,net>
2020-04-15 15:15:17 +0200

    added .dockeringore

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #/bin/sh
 
+mkdir -p /var/www/.texmf-var
+chown www-data:www-data /var/www/.texmf-var
+
 sudo -u www-data mktexpk --mfmode / --bdpi 600 --mag 1+0/600 --dpi 600 ecsx3583
 sudo -u www-data mktexpk --mfmode / --bdpi 600 --mag 1+0/600 --dpi 600 ectt1095
 sudo -u www-data mktexpk --mfmode / --bdpi 600 --mag 1+0/600 --dpi 600 ecss1095
