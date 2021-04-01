c4f04fead252ecc7814009e9b6e9249ab9e2e187
Kristoffer Ek <stoffer@skulp.net>
2021-04-01 21:45:27 +0200

    run smsd as user smsd

--- docker-entrypoint_smsd.sh
+++ docker-entrypoint_smsd.sh
@@ -1,15 +1,6 @@
 #/bin/sh
 
-#PSEUDO_TTY=/var/run/ttyV0
-
-mkdir -p /var/spool/sms/checked &&
-mkdir -p /var/spool/sms/failed &&
-mkdir -p /var/spool/sms/incoming &&
-mkdir -p /var/spool/sms/outgoing &&
-mkdir -p /var/spool/sms/sent &&
-chown -R smsd:smsd /var/spool/sms &&
-
 /smtp_server.pl &
-smsd -c/etc/smsd.conf
+sudo -u smsd smsd -c/etc/smsd.conf
 touch /var/log/smstools/smsd.log
 tail -f /var/log/smstools/smsd.log
