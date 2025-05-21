7940790ff322653379f5f68cfa887ba1d18838f3
Kristoffer Ek <stoffer@skulp.net>
2025-05-21 22:05:04 +0200

    typo and update

--- nabovarme_setup.sh
+++ nabovarme_setup.sh
@@ -2,7 +2,7 @@
 
 mysql -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD nabovarme < /nabovarme.sql &&
 
-echo "GRANT SELECT,INSERT,UPDATE,DELETE ON root.* TO *@'%' IDENTIFIED BY '$MYSQL_PASSWORD'" | mysql -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD &&
+echo "GRANT SELECT,INSERT,UPDATE,DELETE ON root.* TO '*'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'" | mysql -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD &&
 echo "GRANT SELECT,INSERT,UPDATE,DELETE ON nabovarme.* TO 'nabovarme'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'" | mysql -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD &&
 
 echo "FLUSH PRIVILEGES" | mysql -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD
