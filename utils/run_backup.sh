45fa1c947731deb9abcd3e7ef20ecae18c44ef2a
Kristoffer Ek <stoffer@skulp.net>
2025-05-27 23:25:43 +0200

    Update run_backup.sh

--- utils/run_backup.sh
+++ utils/run_backup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 #mysqldump --single-transaction --skip-add-drop-table --no-create-info nabovarme | pbzip2 > /var/lib/mysql_backup.sql.bz2
-mysqldump --single-transaction nabovarme | pbzip2 > /var/lib/mysql_backup.sql.bz2
+mysqldump --single-transaction nabovarme | pbzip2 > mysql_backup.sql.bz2
 if [ "${PIPESTATUS[0]}" -eq "0" ]
 then
 	echo "dump worked"
-	ssh elprog "rsync -av mysql_backup.sql.bz2 mysql_backup_`date +'%d_%m_%Y'`.sql.bz2"
-	rsync -azxv --partial --progress --stats \
-		/var/lib/mysql_backup.sql.bz2 \
-		elprog:
+#	ssh elprog "rsync -av mysql_backup.sql.bz2 mysql_backup_`date +'%d_%m_%Y'`.sql.bz2"
+#	rsync -azxv --partial --progress --stats \
+#		/var/lib/mysql_backup.sql.bz2 \
+#		elprog:
+
 #	rsync -azxv --partial --progress --stats --delete \
 #		--exclude='/var/lib/mysql/' \
 #		--exclude='/root/.ssh/' \
