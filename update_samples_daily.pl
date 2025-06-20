0021cbf3d61d1d8eda972e2d3a5d5ad9d71f4749
Kristoffer Ek <stoffer@skulp.net>
2025-06-20 06:11:21 +0200

    Update update_samples_daily.pl
    run for each serial at a time

--- update_samples_daily.pl
+++ update_samples_daily.pl
@@ -9,7 +9,9 @@ use Time::HiRes qw( usleep );
 use lib qw( /etc/apache2/perl );
 use Nabovarme::Db;
 
-warn("starting...\n");
+$| = 1;	# Autoflush STDOUT
+
+print "starting...\n";
 
 my $dbh;
 my $sth;
@@ -20,10 +22,10 @@ if ($dbh = Nabovarme::Db->my_connect) {
 	$dbh->{'mysql_auto_reconnect'} = 1;
 	$dbh->{'AutoCommit'} = 0;
 	$dbh->{'RaiseError'} = 1;
-	warn("connected to db\n");
+	print "connected to db\n";
 }
 else {
-	warn("cant't connect to db $!\n");
+	print "cant't connect to db $!\n";
 	die $!;
 }
 
@@ -32,34 +34,43 @@ $dbh->disconnect();
 
 
 sub update_samples_daily {
-	warn("update samples_daily for all meters\n");
+	$sth = $dbh->prepare(qq[SELECT `serial`, `key` FROM meters WHERE `key` is not NULL]);
+	$sth->execute;
+	
+	print "cleaning samples_cache for all meters\n";
+	while ($d = $sth->fetchrow_hashref) {
+		my $quoted_serial = $dbh->quote($d->{serial});
 
-	# re calculate in transaction
-	eval {
-		$dbh->do(qq[DELETE FROM samples_daily]) || warn "$!\n";
-		$dbh->do(qq[
-			INSERT INTO samples_daily (
-				`serial`, heap, flow_temp, return_flow_temp, temp_diff,
-				t3, flow, effect, hours, volume, energy, unix_time
-			)
-			SELECT 
-				`serial`, heap, flow_temp, return_flow_temp, temp_diff,
-				t3, flow, effect, hours, volume, energy, unix_time
-			FROM (
+		print "\tcleaning samples_cache for " . $d->{serial} . "\n";
+	
+		# re calculate in transaction
+		eval {
+			$dbh->do(qq[DELETE FROM samples_daily]) || warn "$!\n";
+			$dbh->do(qq[
+				INSERT INTO samples_daily (
+					`serial`, heap, flow_temp, return_flow_temp, temp_diff,
+					t3, flow, effect, hours, volume, energy, unix_time
+				)
 				SELECT 
-					s.*,
-					ROW_NUMBER() OVER (
-						PARTITION BY `serial`, DATE(FROM_UNIXTIME(unix_time))
-						ORDER BY unix_time ASC
-					) AS rn
-				FROM samples s
-			) sub
-			WHERE rn = 1]) or warn "$!\n";
-		$dbh->commit();
-	};
-	if ($@) {
-		warn "Error inserting the link and tag: $@\n";
-		$dbh->rollback();
+					`serial`, heap, flow_temp, return_flow_temp, temp_diff,
+					t3, flow, effect, hours, volume, energy, unix_time
+				FROM (
+					SELECT 
+						s.*,
+						ROW_NUMBER() OVER (
+							PARTITION BY `serial`, DATE(FROM_UNIXTIME(unix_time))
+							ORDER BY unix_time ASC
+						) AS rn
+					FROM samples s
+					WHERE serial = $quoted_serial
+				) sub
+				WHERE rn = 1]) or warn "$!\n";
+			$dbh->commit();
+		};
+		if ($@) {
+			warn "Error inserting the link and tag: $@\n";
+			$dbh->rollback();
+		}
 	}
 }
 
