7f3b0f7c17e84ccd2462a29bdd6059f276176e98
Kristoffer Ek <stoffer@skulp.net>
2025-12-07 20:18:41 +0100

    Migrate SMTP server to Net::Server::Mail::SMTP
    - Replace Net::SMTP::Server/Client with Net::Server::Mail::SMTP and IO::Socket::INET
    - Add libnet-server-mail-perl to Dockerfile dependencies
    - Implement proper RCPT and DATA callbacks for SMS forwarding
    - Use $smtp->reply(421, ...) on SMS failure instead of unsupported methods
    - Clean up LWP headers, JSON payload formatting, and cookie handling

--- smsd/smtp_server.pl
+++ smsd/smtp_server.pl
@@ -7,8 +7,8 @@ use Encode qw(encode decode);
 use Email::Simple;
 use Data::Dumper;
 
-use Net::SMTP::Server;
-use Net::SMTP::Server::Client;
+use Net::Server::Mail::SMTP;
+use IO::Socket::INET;
 
 use LWP::UserAgent;
 use HTTP::Cookies;
@@ -28,14 +28,14 @@ my $password = $ENV{DLINK_ROUTER_PASS} // "";
 # --- Initialize HTTP client for SMS ---
 my $cookie_jar = HTTP::Cookies->new;
 my $ua = LWP::UserAgent->new(
-	agent	  => "Mozilla/5.0",
-	cookie_jar => $cookie_jar,
-	timeout	=> 10,
+	agent	   => "Mozilla/5.0",
+	cookie_jar  => $cookie_jar,
+	timeout	 => 10,
 );
-$ua->default_header("Accept"		 => "application/json, text/javascript, */*; q=0.01");
-$ua->default_header("Accept-Language"=> "en-GB,en;q=0.9");
-$ua->default_header("Connection"	 => "keep-alive");
-$ua->default_header("X-Requested-With"=> "XMLHttpRequest");
+$ua->default_header("Accept"		   => "application/json, text/javascript, */*; q=0.01");
+$ua->default_header("Accept-Language"  => "en-GB,en;q=0.9");
+$ua->default_header("Connection"	   => "keep-alive");
+$ua->default_header("X-Requested-With" => "XMLHttpRequest");
 
 # --- Define SMS sending function ---
 sub send_sms {
@@ -60,17 +60,14 @@ sub send_sms {
 	);
 	die "Login failed\n" unless $login->is_success;
 
-	# Extract qSessId and apply JS-style cookies
+	# Extract qSessId
 	my ($qsess) = $login->header("Set-Cookie") =~ /qSessId=([^;]+)/;
 	die "qSessId not found\n" unless $qsess;
-	$cookie_jar->set_cookie(0, "qSessId",	   $qsess, "/", $router);
-	$cookie_jar->set_cookie(0, "DWRLOGGEDID",   $qsess, "/", $router);
-	$cookie_jar->set_cookie(0, "DWRLOGGEDUSER", "admin", "/", $router);
-	$cookie_jar->set_cookie(0, "DWRLOGGEDTIMEOUT", 300, "/", $router);
+	$cookie_jar->set_cookie(0, "qSessId",	 $qsess, "/", $router);
+	$cookie_jar->set_cookie(0, "DWRLOGGEDID", $qsess, "/", $router);
 
 	# --- STEP 3: GET AUTHID ---
-	my $auth_resp = $ua->get("http://$router/data.ria?token=1",
-							  Referer => "http://$router/controlPanel.html");
+	my $auth_resp = $ua->get("http://$router/data.ria?token=1");
 	die "Failed to get authID\n" unless $auth_resp->is_success;
 	my $authID = $auth_resp->decoded_content;
 	$authID =~ s/\s+//g;
@@ -81,60 +78,71 @@ sub send_sms {
 	$ua->default_header("X-Csrf-Token" => $csrf);
 
 	my $payload = {
-		CfgType	=> "sms_action",
-		type	   => "sms_send",
-		msg		=> $message,
-		phone_list => $phone,
-		authID	 => $authID
+		CfgType	 => "sms_action",
+		type		=> "sms_send",
+		msg		 => $message,
+		phone_list  => $phone,
+		authID	  => $authID
 	};
 	my $json = encode_json($payload);
 
 	my $sms = $ua->post(
 		"http://$router/webpost.cgi",
 		Content_Type => "application/x-www-form-urlencoded; charset=UTF-8",
-		Content	  => $json,
-		Referer	  => "http://$router/controlPanel.html",
-		Origin	   => "http://$router"
+		Content	  => $json
 	);
 
 	die "SMS HTTP failed: " . $sms->code unless $sms->is_success;
 	my $resp = $sms->decoded_content;
 
-	# Check for success
 	if ($resp =~ /"cmd_status":"Done"/ && $resp =~ /"msgSuccess":"1"/) {
-		return 1; # SMS sent
+		return 1;
 	} else {
 		die "SMS gateway error: $resp";
 	}
 }
 
-# --- Start SMTP server ---
-my $server = Net::SMTP::Server->new('0.0.0.0', 25)
-	or croak("Unable to bind SMTP server: $!\n");
+# --- SMTP server using Net::Server::Mail::SMTP ---
+my $socket = IO::Socket::INET->new(
+	LocalPort => 25,
+	Listen	=> 5,
+	Proto	 => 'tcp',
+	Reuse	 => 1,
+) or die "Unable to bind SMTP server port 25: $!";
 
-while (my $conn = $server->accept()) {
-	my $client = Net::SMTP::Server::Client->new($conn)
-		or croak("Unable to handle client: $!\n");
+print "SMTP SMS Gateway running on port 25...\n";
 
-	$client->process || next;
+while (my $client = $socket->accept()) {
+	my $smtp = Net::Server::Mail::SMTP->new(socket => $client);
 
-	for my $to (@{$client->{TO}}) {
-		my $destination = $to;
-		$destination =~ s/\D//g;
+	$smtp->set_callback(RCPT => sub {
+		my ($session, $rcpt) = @_;
+		$rcpt =~ s/\D//g;
+		$session->{_sms_to} = $rcpt;
+		return 1;
+	});
 
-		my $email = Email::Simple->new($client->{MSG});
+	$smtp->set_callback(DATA => sub {
+		my ($session, $data) = @_;
+
+		my $email = Email::Simple->new($data);
 		my $subject = $email->header('Subject') || '';
 		my $body	= $email->body || '';
 		my $message = ($subject && $body) ? "$subject $body" : ($subject . $body);
+		my $dest	= $session->{_sms_to};
 
-		print "Sending SMS to $destination ...\n";
+		print "Sending SMS to $dest ...\n";
+		my $ok = eval { send_sms($dest, $message) };
 
-		my $ok = eval { send_sms($destination, $message) };
 		if ($ok) {
-			print "✔ SMS to $destination sent successfully\n";
+			print "✔ SMS to $dest sent successfully\n";
+			return 1;
 		} else {
-			warn "❌ SMS to $destination failed: $@\n";
-			$client->error("421 SMS gateway temporarily down. Please try again later.");
+			warn "❌ SMS to $dest failed: $@\n";
+			$smtp->reply(421, "SMS gateway temporarily down");
+			return 0;
 		}
-	}
+	});
+
+	$smtp->process || next;
 }
