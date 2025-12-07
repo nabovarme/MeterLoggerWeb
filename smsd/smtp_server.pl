bbc25a4bc6598b594c7bfd4f892e38a9daa1b3ff
Kristoffer Ek <stoffer@skulp.net>
2025-12-07 09:12:36 +0100

    Integrate D-Link DWR-932 SMS sending into SMTP server
        - Added environment variables DLINK_ROUTER_IP, DLINK_ROUTER_USER, DLINK_ROUTER_PASS
          in .env.example for Docker Compose configuration.
        - Updated docker-compose.yml to pass D-Link DWR-932 router environment variables to smsd service.
        - Added required Perl modules (libwww-perl, libjson-perl) in smsd/Dockerfile.
        - Commented out smsd_watchdog service from docker-compose.yml.
        - Refactored smtp_server.pl:
          * Added send_sms() function to send SMS via D-Link DWR-932 router using environment variables.
          * Integrated SMS sending for every received email.
          * Added temporary rejection (421) if SMS fails to ensure emails are not discarded.
          * Removed old spooling mechanism and UCS-2BE encoding; sending is now direct via router API.
        - Updated docker-entrypoint.sh to remove smsd launch (now handled via SMTP server integration).
    T

--- smsd/smtp_server.pl
+++ smsd/smtp_server.pl
@@ -1,74 +1,135 @@
 #!/usr/bin/perl -w
 
 use strict;
+use warnings;
 use Carp;
-
-use File::Temp qw( tempfile );
-use File::Basename;
-use File::Copy;
-use File::chown;
-use Encode qw( encode decode );
+use Encode qw(encode decode);
+use Email::Simple;
+use Data::Dumper;
 
 use Net::SMTP::Server;
 use Net::SMTP::Server::Client;
-use Net::SMTP::Server::Relay;
 
-use Email::Simple;
+use LWP::UserAgent;
+use HTTP::Cookies;
+use Digest::MD5 qw(md5_hex);
+use JSON;
 
-use Data::Dumper;
-
-use constant SPOOL_DIR => '/var/spool/sms/outgoing';
-#use constant SPOOL_DIR => '/tmp';
-use constant USER => 'smsd';
+use constant USER  => 'smsd';
 use constant GROUP => 'smsd';
 
+# --- Read configuration from environment ---
+my $router   = $ENV{DLINK_ROUTER_IP}   or die "Missing DLINK_ROUTER_IP env variable\n";
+my $username = $ENV{DLINK_ROUTER_USER} or die "Missing DLINK_ROUTER_USER env variable\n";
+my $password = $ENV{DLINK_ROUTER_PASS} // "";
 
-my $server = new Net::SMTP::Server('0.0.0.0', 25) ||
-	croak("Unable to handle client connection: $!\n");
+# --- Initialize HTTP client for SMS ---
+my $cookie_jar = HTTP::Cookies->new;
+my $ua = LWP::UserAgent->new(
+	agent	  => "Mozilla/5.0",
+	cookie_jar => $cookie_jar,
+	timeout	=> 10,
+);
+$ua->default_header("Accept"		 => "application/json, text/javascript, */*; q=0.01");
+$ua->default_header("Accept-Language"=> "en-GB,en;q=0.9");
+$ua->default_header("Connection"	 => "keep-alive");
+$ua->default_header("X-Requested-With"=> "XMLHttpRequest");
+
+# --- Define SMS sending function ---
+sub send_sms {
+	my ($phone, $message) = @_;
+	die "Missing phone or message\n" unless $phone && $message;
+
+	# --- STEP 1: INIT SESSION ---
+	my $init = $ua->get("http://$router/index.html");
+	die "Failed to init session\n" unless $init->is_success;
+
+	# --- STEP 2: LOGIN ---
+	my $md5pass = md5_hex($password);
+	my $login = $ua->post(
+		"http://$router/login.cgi",
+		Content_Type => "application/x-www-form-urlencoded; charset=UTF-8",
+		Content	  => "uname=$username&passwd=$md5pass",
+		Referer	  => "http://$router/index.html",
+		Origin	   => "http://$router"
+	);
+	die "Login failed\n" unless $login->is_success;
+
+	# Extract qSessId and apply JS-style cookies
+	my ($qsess) = $login->header("Set-Cookie") =~ /qSessId=([^;]+)/;
+	die "qSessId not found\n" unless $qsess;
+	$cookie_jar->set_cookie(0, "qSessId",	   $qsess, "/", $router);
+	$cookie_jar->set_cookie(0, "DWRLOGGEDID",   $qsess, "/", $router);
+	$cookie_jar->set_cookie(0, "DWRLOGGEDUSER", "admin", "/", $router);
+	$cookie_jar->set_cookie(0, "DWRLOGGEDTIMEOUT", 300, "/", $router);
+
+	# --- STEP 3: GET AUTHID ---
+	my $auth_resp = $ua->get("http://$router/data.ria?token=1",
+							  Referer => "http://$router/controlPanel.html");
+	die "Failed to get authID\n" unless $auth_resp->is_success;
+	my $authID = $auth_resp->decoded_content;
+	$authID =~ s/\s+//g;
+	die "Empty authID\n" unless $authID;
+
+	# --- STEP 4: SEND SMS ---
+	my $csrf = sprintf("%06d", int(rand(999_999)));
+	$ua->default_header("X-Csrf-Token" => $csrf);
+
+	my $payload = {
+		CfgType	=> "sms_action",
+		type	   => "sms_send",
+		msg		=> $message,
+		phone_list => $phone,
+		authID	 => $authID
+	};
+	my $json = encode_json($payload);
+
+	my $sms = $ua->post(
+		"http://$router/webpost.cgi",
+		Content_Type => "application/x-www-form-urlencoded; charset=UTF-8",
+		Content	  => $json,
+		Referer	  => "http://$router/controlPanel.html",
+		Origin	   => "http://$router"
+	);
+
+	die "SMS HTTP failed: " . $sms->code unless $sms->is_success;
+	my $resp = $sms->decoded_content;
+
+	# Check for success
+	if ($resp =~ /"cmd_status":"Done"/ && $resp =~ /"msgSuccess":"1"/) {
+		return 1; # SMS sent
+	} else {
+		die "SMS gateway error: $resp";
+	}
+}
+
+# --- Start SMTP server ---
+my $server = Net::SMTP::Server->new('0.0.0.0', 25)
+	or croak("Unable to bind SMTP server: $!\n");
 
 while (my $conn = $server->accept()) {
-	my $client = new Net::SMTP::Server::Client($conn) ||
-		croak("Unable to handle client connection: $!\n");
+	my $client = Net::SMTP::Server::Client->new($conn)
+		or croak("Unable to handle client: $!\n");
 
 	$client->process || next;
-#	print Dumper $client;
 
-	for (@{$client->{TO}}) {
-		# destination and message text
-		my $destination = $_;
+	for my $to (@{$client->{TO}}) {
+		my $destination = $to;
 		$destination =~ s/\D//g;
-		
+
 		my $email = Email::Simple->new($client->{MSG});
 		my $subject = $email->header('Subject') || '';
-		my $body = $email->body || '';
-		my $message = ($subject && $body) ? ($subject . ' ' . $body) : ($subject . $body);
-#		print Dumper $email->header('To');
-#		print Dumper ($destination, $message);
+		my $body	= $email->body || '';
+		my $message = ($subject && $body) ? "$subject $body" : ($subject . $body);
 
-		# convert message from UTF-8 to UCS
-		$message = encode('UCS-2BE', decode('UTF-8', $message));
-		$message =~ s/\r$//;	# remove trailing carriage return
+		print "Sending SMS to $destination ...\n";
 
-		my ($fh, $temp_file) = tempfile();
-		#binmode( $fh, ":utf8" );
-		chown USER, GROUP, $temp_file;
-
-		print $fh "To: " . $destination . "\n";
-		print $fh "Alphabet: UCS\n";
-		print $fh "\n";
-		print $fh $message;
-		close($fh);
-
-		my $message_file = SPOOL_DIR . '/' . $destination . '_' . basename($temp_file);
-		my $lock_file = $message_file . '.LOCK';
-		open(LOCK_FILE, ">>" . $lock_file) || die "Cannot open file: " . $!;
-		close(LOCK_FILE);
-		chown USER, GROUP, $lock_file;
-
-		move($temp_file, $message_file) || die $!;
-		chown USER, GROUP, $message_file;
-
-		unlink $lock_file;		
+		my $ok = eval { send_sms($destination, $message) };
+		if ($ok) {
+			print "✔ SMS to $destination sent successfully\n";
+		} else {
+			warn "❌ SMS to $destination failed: $@\n";
+			$client->reject(421 => "SMS gateway temporarily down. Please try again later.");
+		}
 	}
-
 }
