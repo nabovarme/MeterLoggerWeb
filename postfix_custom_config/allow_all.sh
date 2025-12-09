e903fca804c402d5c0793597b23cff2339d6b050
Kristoffer Ek <stoffer@skulp.net>
2025-12-09 02:54:08 +0100

    tune postfix queue timings for rapid retries
    - set `queue_run_delay` to 10s
    - set `minimal_backoff_time` to 10s
    - set `maximal_backoff_time` to 60s
    - reload postfix after applying settings

--- postfix_custom_config/allow_all.sh
+++ postfix_custom_config/allow_all.sh
@@ -2,3 +2,6 @@
 
 postconf -e 'smtpd_recipient_restrictions = check_sender_access lmdb:/etc/postfix/allowed_senders, reject'
 postconf -e 'smtpd_helo_restrictions = permit_mynetworks'
+postconf -e "queue_run_delay = 10s"
+postconf -e "minimal_backoff_time = 10s"
+postconf -e "maximal_backoff_time = 60s"
