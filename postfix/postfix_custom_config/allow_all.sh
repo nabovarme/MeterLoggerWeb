159a4163517700a81acb18bd57f2d760fb818657
Kristoffer Ek <stoffer@skulp.net>
2025-12-19 14:30:24 +0100

    postfix: add default_destination_rate_delay to slow down mail delivery
    Set `default_destination_rate_delay` to 10s in allow_all.sh to introduce a pause between
    mails sent to the same destination, helping to prevent overloading relay hosts.

--- postfix/postfix_custom_config/allow_all.sh
+++ postfix/postfix_custom_config/allow_all.sh
@@ -5,3 +5,4 @@ postconf -e 'smtpd_helo_restrictions = permit_mynetworks'
 postconf -e "queue_run_delay = 10s"
 postconf -e "minimal_backoff_time = 10s"
 postconf -e "maximal_backoff_time = 60s"
+postconf -e "default_destination_rate_delay = 10s"
