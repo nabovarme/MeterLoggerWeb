2bcad072b0743ad55d8e7d5f47a28e3ebfb15470
Kristoffer Ek <stoffer@skulp.net>
2026-05-22 02:31:09 +0200

    Update Db.pm
    cache db connection

--- perl/Nabovarme/Db.pm
+++ perl/Nabovarme/Db.pm
@@ -2,14 +2,30 @@ package Nabovarme::Db;
 
 use strict;
 use warnings;
+use utf8;
 use DBI;
 
+# Keep a persistent lexical handle cache across requests in the mod_perl worker
+our $cached_dbh;
+
 sub my_connect {
-	my $dbh = DBI->connect(
+	# 1. Fast path: check if our global connection is alive and kicking
+	if ($cached_dbh && $cached_dbh->ping) {
+		return $cached_dbh;
+	}
+
+	# 2. Slow path: connect_cached automatically creates or revives identical handles
+	my $dbh = DBI->connect_cached(
 		'dbi:mysql:',
 		'',
 		'',
-		{ mysql_read_default_file => '/etc/mysql/my.cnf', mysql_auto_reconnect => 1, mysql_enable_utf8mb4 => 1 }
+		{ 
+			mysql_read_default_file => '/etc/mysql/my.cnf', 
+			mysql_auto_reconnect    => 1, 
+			mysql_enable_utf8mb4    => 1,
+			PrintError              => 0,
+			RaiseError              => 0,
+		}
 	);
 	
 	unless ($dbh) {
@@ -17,6 +33,7 @@ sub my_connect {
 		return undef;
 	}
 	
+	$cached_dbh = $dbh;
 	return $dbh;
 }
 
