ffb0587c49707cd8958c4b695ef28375fd9c6168
Kristoffer Ek <stoffer@skulp.net>
2026-07-05 18:58:24 +0200

    refactor(sdk): simplify entrypoint to leverage new multi-segment make release target
    and upgraded to mariadb 12.3

--- builder/firmware_sdk/docker-entrypoint.sh
+++ builder/firmware_sdk/docker-entrypoint.sh
@@ -20,8 +20,8 @@ echo "BUILD_FLAGS=$BUILD_FLAGS"
 # Go to repo
 cd /meterlogger/MeterLogger
 
-# Build firmware and components
-make clean all \
+# FIXED: Leverage the newly refactored release rule to build and copy all segments atomically
+make clean release \
 	$BUILD_FLAGS \
 	SERIAL=$SERIAL \
 	KEY=$KEY
@@ -32,13 +32,6 @@ if [ $EXIT_CODE -ne 0 ]; then
 	echo "Build failed with exit code $EXIT_CODE"
 	exit $EXIT_CODE
 fi
-
-echo "Copy files into the mapped release/ tracking path..."
-cp firmware/0x00000.bin release/0x00000.bin
-cp firmware/0x10000.bin release/0x10000.bin
-cp webpages.espfs release/webpages.espfs
-cp firmware/esp_init_data_default_112th_byte_0x03.bin release/esp_init_data_default_112th_byte_0x03.bin
-cp firmware/blank.bin release/blank.bin
 
 echo "Build completed successfully"
 exit 0
